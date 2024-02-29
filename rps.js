c6a01ad7771297fe20198684c9251d892c91780e
Kelly <126833824+kraftwerkit@users.noreply.github.com>
2024-02-29 15:47:38 -0800

    Functional buttons and display

--- rps.js
+++ rps.js
@@ -1,20 +1,14 @@
-/*Pseudocode
+/* What do I want to do?
+1. User picks their selection via a button.
+2. Compiter will generate its choice.
+3. Implement logic to declare a winner or tie for the round.
+4. Do not add points if a tie.
+4. Display the running score.
+5. Keep doing the above until a player reaches 5 points.
+6. Announce the winner.*/
 
-Step 1: Ask the user for their choice.
-
-Step 2: Ask the computer for their choice.
-
-Step 3: Declare the winner (or tie).
-
-Step 4: Add a point to the winner's count.
-
-Step 5: Repeat steps 1-5 four more times.
-
-Step 6: Report the winner and final score.
-*/
-
-
-/*Get the Computer Choice */
+/* Gets the computer's choice*/
+let computerSelection=getComputerChoice()
 
 function getComputerChoice () {
     let random = Math.floor(Math.random() * 61);
@@ -31,56 +25,39 @@ function getComputerChoice () {
     }
 }
 
-/*A single round of RPS */
+/* Gets the player's Choice */
+const rock = document.querySelector("#Rock")
+const paper = document.querySelector("#Paper")
+const scissors = document.querySelector("#Scissors")
+
+/* Displays round result */
+const display = document.querySelector("#Display");
+
+
+paper.addEventListener("click", () => {
+    playRound("Paper", computerSelection);})
+
+rock.addEventListener("click", () => {
+    playRound("Rock", computerSelection);})
+
+scissors.addEventListener("click", () => {
+    playRound("Scissors", computerSelection);})
+
+    
 function playRound (playerSelection,computerSelection) {
-playerSelection = prompt("Rock, Paper or Scissors?");
-
-let finalPlayerSelection = playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1).toLowerCase();
-
-computerSelection = getComputerChoice();
-if (finalPlayerSelection === computerSelection) {
-    return `It's a tie! You both picked ${finalPlayerSelection} You: ${playerScore} Computer: ${computerScore} `;
-}
-
-else {
-    if ((finalPlayerSelection === "Rock" && computerSelection === "Scissors") || 
-        (finalPlayerSelection === "Paper" && computerSelection === "Rock") ||
-        (finalPlayerSelection === "Scissors" && computerSelection === "Paper")) {
-            playerScore++
-            return `You win! ${finalPlayerSelection} beats ${computerSelection} You: ${playerScore} Computer: ${computerScore}`;
-
+    computerSelection = getComputerChoice();
+    if (playerSelection === computerSelection) {
+        display.textContent=`It's a tie! You both picked ${playerSelection}`;
+    }
+        
+    else if ((playerSelection === "Rock" && computerSelection === "Scissors") || (playerSelection === "Paper" && computerSelection === "Rock") ||
+            (playerSelection === "Scissors" && computerSelection === "Paper")) 
+            
+            {
+                display.textContent= `You win! ${playerSelection} beats ${computerSelection}`;
         }
-
+        
     else {
-        computerScore++
-        return `You lose! ${computerSelection} beats ${finalPlayerSelection} You: ${playerScore} Computer: ${computerScore}`
-    }
-}
-}
-
-/* Game of 5 Rounds */
-let playerScore = 0 /*Scores are global variables*/
-let computerScore = 0
-
-function playGame() {
-    console.log(playRound())
-    console.log(playRound())
-    console.log(playRound())
-    console.log(playRound())
-    console.log(playRound())
-    
-    if (computerScore > playerScore) {
-       console.log("The computer wins!");
-    }
-
-    else if (computerScore === playerScore) {
-        console.log("It's a tie!");
-
-    }
-
-    else {
-        console.log("You win!");
-    }
-    }
-    
-playGame()
+        display.textContent= `You lose! ${computerSelection} beats ${playerSelection}`
+        }
+        }
